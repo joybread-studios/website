d10e3cea554ea8ba049d237a99bae29368cbed92
Shinn <sunikhedge@gmail.com>
2023-10-29 01:51:02 +0800

    fixed metadata

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -7,7 +7,7 @@ export default function Header() {
     return(
         <div className="p-4 bg-black flex fixed w-full border-b justify-between items-center">
             <div className='px-2 flex flex-col justify-center items-center'>
-                <Link href='/' className='flex flex-col items-center text-xs font-bold'><Image src='/joybreadstudios.png' alt='' height={64} width={64}/>Joybread Studios</Link>
+                <Link href='/' className='flex flex-col items-center text-xs font-bold'><Image src='/joybreadstudios-nobg.png' alt='' height={64} width={64}/>Joybread Studios</Link>
             </div>
             <div>
                 <NavLink href='#intro'>HOME</NavLink>
--- src/app/templates/AboutSection.js
+++ src/app/templates/AboutSection.js
@@ -3,7 +3,7 @@ export default function AboutSection () {
         <div className='h-screen w-full flex flex-col' id='about'>
             <p className='p-8 text-4xl font-bold text-yellow-300'>ABOUT</p>
             <div className="mx-8 p-8 border-t">
-                <p>We are a Philippines-based company.</p>
+                <p>We are a Filipino-based company.</p>
             </div>
         </div>
     )
